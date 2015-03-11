4c558de314ae855c528d3fee4c552b10648be9eb
nikitasmall <nikitasosnov@yahoo.com>
2015-03-11 22:38:40 +0200

    Init commit!

---
+++ todo.py
@@ -0,0 +1,18 @@
+__author__ = 'nikitasmall'
+
+import sqlite3
+from bottle import route, run, debug, template
+
+
+@route('/todo')
+def todo_list():
+    conn = sqlite3.connect("todo.db")
+    c = conn.cursor()
+    c.execute("SELECT id, task FROM todo WHERE status LIKE '1'")
+    result = c.fetchall()
+    c.close()
+    output = template('todo', rows=result)
+    return output
+
+debug(True)
+run(reloader=True)
