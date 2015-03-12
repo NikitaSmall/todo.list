77312badb7cb644d3a806382a0c9142490c5d351
nikitasmall <nikitasosnov@yahoo.com>
2015-03-12 23:13:25 +0200

    bootstrap added

--- todo.py
+++ todo.py
@@ -1,7 +1,19 @@
 __author__ = 'nikitasmall'
 
 import sqlite3
-from bottle import route, run, debug, template
+from bottle import route, run, debug, template, static_file, view
+
+@route('/js/:filename')
+def js_static(filename):
+    return static_file(filename, root='./views/js')
+
+@route('/img/:filename')
+def img_static(filename):
+    return static_file(filename, root='./views/img')
+
+@route('/css/:filename')
+def css_static(filename):
+    return static_file(filename, root='./views/css')
 
 
 @route('/todo')
@@ -11,7 +23,7 @@ def todo_list():
     c.execute("SELECT id, task FROM todo WHERE status LIKE '1'")
     result = c.fetchall()
     c.close()
-    output = template('todo', rows=result)
+    output = template('views/todo', rows=result)
     return output
 
 debug(True)
